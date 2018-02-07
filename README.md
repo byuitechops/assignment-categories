65b5824e6979fbbd24bab2e2e68c0182e8c1b499
sethchilders92 <sethchilders92@gmail.com>
2018-02-07 13:16:43 -0700

    Updated README.md and added a course.log to main.js

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
 # Assignment Categories
-### Package Name: assignment-categories
-### Child Type: post import
+### *Package Name*: assignment-categories
+### *Child Type*: post import
+### *Platform*: all
+### *Required*: Recommended
 
-This child module is built to be used by the Brigham Young University - Idaho D2L to Canvas Conversion Tool. It utilizes the standard `module.exports => (course, stepCallback)` signature and uses the Conversion Tool's standard logging functions.
+This child module is built to be used by the Brigham Young University - Idaho D2L to Canvas Conversion Tool. It utilizes the standard `module.exports => (course, stepCallback)` signature and uses the Conversion Tool's standard logging functions. You can view extended documentation [Here](https://github.com/byuitechops/d2l-to-canvas-conversion-tool/tree/master/documentation).
 
 ## Purpose
 
 When a course is imported from D2L, if its grading system uses 'Weighted' grades, then these weights are imported automatically into Canvas. These grade weights, however, are not used until a setting is changed that tells Canvas to use the weights to calculate the final grade. 
 
-This child module is meant to address this issue by changing the setting in Canvas to use the weights if the grading system from D2L also used weights. This child module will also check to see if the grade weights add up to 100%, and will throw an error if they do not.
+This child module is meant to address this issue by changing the setting in Canvas to use the weights if the grading system from D2L also used weights. This child module will also check to see if the grade weights add up to 100%, and will throw a warning if they do not.
+
+## How to Install
+
+```
+npm install assignment-categories
+```
+
+## Run Requirements
+
+None 
+
+## Options
+
+None
+
+## Outputs
+
+None
 
 ## Process
 
@@ -20,3 +40,21 @@ Describe in steps how the module accomplishes its goals.
 	- Check to see if grade weights add up to 100% and throw a warning if not
 	- Tell Canvas to apply the weights to the assignment groups with a PUT request
 	- Throw an error if the PUT request encounters issues
+4. Else if the grading system is not based on weights
+	- End the child module and move to the next one
+
+## Log Categories
+
+List the categories used in logging data in your module.
+
+- Grading System Used
+
+## Requirements
+
+### The child module shall:
+
+1. Apply the setting on Canvas that applies grade weights to assignment groups if the grading system is set to use grade weights
+
+### The child module may (at a later time):
+
+1. Create an assignment group for each week's assignments if the grading system is set to use anything but grade weights
--- main.js
+++ main.js
@@ -10,80 +10,96 @@ also check to see if the grade weights add up to 100%, and will throw an error i
 const canvas = require('canvas-wrapper');
 
 module.exports = (course, stepCallback) => {
-    /**********************************************
+	/**********************************************
 	 * 	findGradingSystem()				  
 	 *  Parameters: none
 	 **********************************************/
-    function findGradingSystem() {
-        /* Get the contents of 'grades_d2l.xml' */
-        var myFile = course.content.find(file => {
-            return file.name === 'grades_d2l.xml';
-        });
+	function findGradingSystem() {
+		/* Get the contents of 'grades_d2l.xml' */
+		var myFile = course.content.find(file => {
+			return file.name === 'grades_d2l.xml';
+		});
 
-        /* If grades_d2l.xml wasn't found, stop the child module */
-        if (!myFile) {
-            course.warning('Couldn\'t locate \'grades_d2l.xml\' for this course.');
-            stepCallback(null, course);
-        }
+		/* If grades_d2l.xml wasn't found, stop the child module */
+		if (!myFile) {
+			course.warning('Couldn\'t locate \'grades_d2l.xml\' for this course.');
+			stepCallback(null, course);
+		}
 
-        /* Use Cheerio.js to parse through myFile to find the grading system (points (0), weights (1), or based on a custom formula(2)) */
-        var gradingSystem = myFile.dom('configuration>grading_system').html();
+		/* Use Cheerio.js to parse through myFile to find the grading system (points (0), weights (1), or based on a custom formula(2)) */
+		var gradingSystem = myFile.dom('configuration>grading_system').html();
 
-        /* If the grading system uses grade weights, call useWeightedGroups() */
-        if (gradingSystem == 1) {
-            /* Put the grade weight categories in an array to traverse through */
-            var weights = myFile.dom('category>scoring>weight');
-            useWeightedGroups(weights);
+		var name = '';
+		if (gradingSystem == 0) {
+			name = 'points';
+		} else if (gradingSystem == 1) {
+			name = 'weights';
+		} else if (gradingSystem == 2) {
+			name = 'custom defined formula - points are still used';
+		}
 
-        } else {
-            course.message('Course does not use a weighted grade book, moving to next child module');
-            /* Since the course doesn't use weighted grades, skip this child module */
-            stepCallback(null, course);
-        }
-    }
+		/* Log the grading system */
+		course.log(`Grading System Used`, {
+			'grading_system_id': gradingSystem,
+			'grading_system_name': name,
+		});
+
+		/* If the grading system uses grade weights, call useWeightedGroups() */
+		if (gradingSystem == 1) {
+			/* Put the grade weight categories in an array to traverse through */
+			var weights = myFile.dom('category>scoring>weight');
+			useWeightedGroups(weights);
+
+		} else {
+			course.message('Course does not use a weighted grade book, moving to next child module');
+			/* Since the course doesn't use weighted grades, skip this child module */
+			stepCallback(null, course);
+		}
+	}
 
 
 
-    /**********************************************
+	/**********************************************
 	 * 	makeWeightedGroups()				  
 	 *  Parameters: weights
 	 **********************************************/
-    function useWeightedGroups(weights) {
-        /* Add up all the grade weights and ensure they equal 100% */
-        var weightTotal = 0;
+	function useWeightedGroups(weights) {
+		/* Add up all the grade weights and ensure they equal 100% */
+		var weightTotal = 0;
 
-        /* Add up all the grade weights */
-        weights.each((i, weight) => {
-            weightTotal += parseFloat(weight.children[0].data);
-        });
+		/* Add up all the grade weights */
+		weights.each((i, weight) => {
+			weightTotal += parseFloat(weight.children[0].data);
+		});
 
-        /* If the weights don't add up to 100%, throw a warning and move on */
-        if (weightTotal !== 100) {
-            course.warning(`Grade Weights do not add up to 100%, but instead add up to ${weightTotal}%`);
-        }
+		/* If the weights don't add up to 100%, throw a warning and move on */
+		if (weightTotal !== 100) {
+			course.warning(`Grade Weights do not add up to 100%, but instead add up to ${weightTotal}%`);
+		}
 
-        /* Canvas will automatically import the grade weights, but won't use them. 
+
+		/* Canvas will automatically import the grade weights, but won't use them. 
 		This PUT request will tell Canvas to use the weights to calculate the final grade */
-        canvas.put(`/api/v1/courses/${course.info.canvasOU}`, {
-            'course': {
-                'apply_assignment_group_weights': true,
-            }
-        },
-        (putErr) => {
-            if (putErr) {
-                course.error(putErr);
-            } else {
-                course.message('Finished setting final grade to be based on assignment group weights');
-            }
-            /* Finish the child module */
-            stepCallback(null, course);
-        });
-    }
+		canvas.put(`/api/v1/courses/${course.info.canvasOU}`, {
+				'course': {
+					'apply_assignment_group_weights': true,
+				}
+			},
+			(putErr) => {
+				if (putErr) {
+					course.error(putErr);
+				} else {
+					course.message('Finished setting final grade to be based on assignment group weights');
+				}
+				/* Finish the child module */
+				stepCallback(null, course);
+			});
+	}
 
-    /**********************************************
+	/**********************************************
 	 * 				START HERE					  *
 	 **********************************************/
-    /* Start the child module by finding out which grading system is used */
-    findGradingSystem();
+	/* Start the child module by finding out which grading system is used */
+	findGradingSystem();
 
 };
