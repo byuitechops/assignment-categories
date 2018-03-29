21005a2ad321afc4edb0c855279f8eb74aaa00db
Seth Childers <sethchilders92@gmail.com>
2018-03-29 12:52:40 -0600

    Added tests but can't run them

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -1,48 +1,51 @@
-/*eslint-env node, es6*/
-
 /* Dependencies */
 const tap = require('tap');
+const canvas = require('canvas-wrapper');
+const asyncLib = require('async');
 
-function g1Tests(course, callback) {
-    // Tap tests for Gauntlet 1 go here
-    tap.pass('Success! Wheee! 1');
-    // tap.fail('YOLO');
+module.exports = (course, callback) => {
+    if (course.info.courseCode !== 'CG 2') {
+        callback(null, course);
+        return;
+    }
+
+    tap.test('assignment-categories', (test) => {
+        /* Get the contents of 'grades_d2l.xml' */
+        var myFile = course.content.find(file => {
+            return file.name === 'grades_d2l.xml';
+        });
+
+        /* If grades_d2l.xml wasn't found, stop the child module */
+        if (!myFile) {
+            course.warning('Couldn\'t locate \'grades_d2l.xml\' for this course.');
+            stepCallback(null, course);
+        }
+
+        /* Use Cheerio.js to parse through myFile to find the grading system (points (0), weights (1), or based on a custom formula(2)) */
+        var gradingSystem = myFile.dom('configuration>grading_system').html();
+
+        if (gradingSystem !== 1) {
+            test.fail('Grading system should be weights but isn\'t');
+        } else {
+            test.pass('Grading system is correct');
+        }
+
+        var weights = myFile.dom('category>scoring>weight');
+        var weightTotal = 0;
+
+		/* Add up all the grade weights */
+		weights.each((i, weight) => {
+			weightTotal += parseFloat(weight.children[0].data);
+        });
+        
+        if (weightTotal === 50) {
+            test.pass('Weight total adds up correctly');
+        } else {
+            test.fail('Weights should add up to 50% and they don\'t');
+        }
+
+        test.end();
+    });
+
     callback(null, course);
-}
-
-function g2Tests(course, callback) {
-    // Tap tests for Gauntlet 2 go here
-    tap.pass('Success! Wheee! 2');
-    callback(null, course);
-}
-
-function g3Tests(course, callback) {
-    // Tap tests for Gauntlet 3 go here
-    tap.pass('Success! Wheee! 3');
-    callback(null, course);
-}
-
-function g4Tests(course, callback) {
-    // Tap tests for Gauntlet 4 go here
-    tap.pass('Success! Wheee! 4');
-    callback(null, course);
-}
-
-module.exports = [
-        {
-            gauntlet: 1,
-            tests: g1Tests
-        },
-        {
-            gauntlet: 2,
-            tests: g2Tests
-        },
-        {
-            gauntlet: 3,
-            tests: g3Tests
-        },
-        {
-            gauntlet: 4,
-            tests: g4Tests
-        },
-];
+};
